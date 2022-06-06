006417fc5056592e77cf64c5aaee1090a7853389
Aelida <lidasouza89@gmail.com>
2022-06-05 22:07:16 -0300

    Inicialização requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,17 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', async () => {
+    const func = await expect(typeof fetchProducts).toBe('function')
+  });
+  it('Execute a função fetchProducts com o argumento `computador` e teste se fetch foi chamada', async () => {
+    fetchProducts('computador');
+    const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+    await expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
+  it(`Teste se, ao chamar a função 'fetchProducts' com o argumento 'computador', a função fetch utiliza o endpoint 'https://api.mercadolibre.com/sites/MLB/search?q=computador''`, async () => {
+    fetchProducts('computador');
+    const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+    await expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
 });
