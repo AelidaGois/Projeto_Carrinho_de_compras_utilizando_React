6236c66386a98f84f9f45158f1e85772518e79fc
Aelida <lidasouza89@gmail.com>
2022-06-06 16:29:38 -0300

    Alterações requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,16 @@
 const fetchProducts = async (computador) => {
-fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computador}`);
+  if (!fetchProducts) {
+    throw new Error('You must provide an url');
+  }
+
+  try {
+    const endPoint = (`https://api.mercadolibre.com/sites/MLB/search?q=${computador}`);
+    const fetchUrl = await fetch(endPoint);
+    const response = await fetchUrl.json();
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -16,4 +16,15 @@ describe('1 - Teste a função fetchProducts', () => {
     const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
     await expect(fetch).toHaveBeenCalledWith(endPoint);
   });
+  it('Teste se o retorno da função `fetchProducts` com o argumento `computador` é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função `fetchProducts` sem argumento, retorna um erro com a mensagem: `You must provide an url`', async () => {
+    expect(() => {
+      fetchProducts();
+    }).toThrow('You must provide an url');
+  });
+
+
 });
