5df5b322f286f4b5b73c7d42b4ca214a0e240476
Aelida <lidasouza89@gmail.com>
2022-06-09 14:50:31 -0300

    Inclusão requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,7 +5,11 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   it('Teste se, ao executar `getSavedCartItems`, o método `localStorage.getItem` é chamado', async()=> {
-    getSavedCartItems()
-  })
- 
+    await expect (getSavedCartItems()).toBeCalled('localStorage.getItem');
+  });
+
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o `cartItems` como parâmetro.', async()=> {
+    getSavedCartItems();
+    await expect ('localStorage.getItem').toHaveBeenCalledWith(`cartItems`);
+  });
 });
